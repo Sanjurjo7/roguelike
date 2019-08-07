b259726420d5e9f39901ca78c96ef93201f2cb65
Technology Teacher <techteacher@nativitycatholicschool.org>
2019-08-07 16:49:50 -0400

    Added equipment, still buggy

--- entity.py
+++ entity.py
@@ -1,6 +1,8 @@
 import tcod as libtcod
 import math
 
+from components.item import Item
+
 from render_functions import RenderOrder
 
 
@@ -11,7 +13,8 @@ class Entity:
 
     def __init__(self, x, y, char, color, name, blocks=False,
             render_order=RenderOrder.CORPSE, fighter=None, ai=None,
-            item=None, inventory=None, stairs=None, level=None):
+            item=None, inventory=None, stairs=None, level=None,
+            equipment=None, equippable=None):
         self.x = x
         self.y = y
         self.char = char
@@ -25,6 +28,8 @@ class Entity:
         self.inventory = inventory
         self.stairs = stairs
         self.level = level
+        self.equipment = equipment
+        self.equippable = equippable
 
         if self.fighter:
             self.fighter.owner = self
@@ -40,6 +45,20 @@ class Entity:
 
         if self.stairs:
             self.stairs.owner = self
+
+        if self.level:
+            self.level.owner = self
+
+        if self.equipment:
+            self.equipment.owner = self
+
+        if self.equippable:
+            self.equippable.owner = self
+
+            if not self.item:
+                item = Item()
+                self.item = item
+                self.item.owner = self
 
     def move(self, dx, dy):
         # Move the entity by a given amount
--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -2,6 +2,8 @@ import tcod as libtcod
 from random import randint
 
 from components.ai import BasicMonster
+from components.equipment import EquipmentSlots
+from components.equippable import Equippable
 from components.fighter import Fighter
 from components.item import Item
 from components.stairs import Stairs
@@ -131,6 +133,9 @@ class GameMap:
         }
         item_chances = {
             'healing_potion': 35,
+            'dirk': from_dungeon_level([[10, 2]], self.dungeon_level),
+            'sword': from_dungeon_level([[5, 4]], self.dungeon_level),
+            'shield': from_dungeon_level([[15,8]], self.dungeon_level),
             'lightning_scroll': from_dungeon_level([[25, 4]], self.dungeon_level),
             'fireball_scroll': from_dungeon_level([[25, 6]], self.dungeon_level),
             'confusion_scroll': from_dungeon_level([[25, 2]], self.dungeon_level)
@@ -175,6 +180,16 @@ class GameMap:
                     item_component = Item(use_function=heal, amount=40)
                     item = Entity(x, y, '!', libtcod.violet, 'Healing Potion', 
                             render_order=RenderOrder.ITEM, item=item_component)
+
+                elif item_choice == 'dirk':
+                    item_component = Equippable(EquipmentSlots.OFF_HAND, power_bonus=1, defense_bonus=1)
+                    item = Entity(x, y, '-', libtcod.sky, 'Dirk', equippable=equippable_component)
+                elif item_choice == 'sword':
+                    item_component = Equippable(EquipmentSlots.MAIN_HAND, power_bonus=3)
+                    item = Entity(x, y, '/', libtcod.sky, 'Sword', equippable=equippable_component)
+                elif item_choice == 'shield':
+                    item_component = Equippable(EquipmentSlots.OFF_HAND, defense_bonus=2)
+                    item = Entity(x, y, '[', libtcod.darker_orange, 'Shield', equippable=equippable_component)
 
                 elif item_choice == 'fireball_scroll':
                     item_component = Item(use_function=cast_fireball, targeting=True, targeting_message=Message(
